7adc3d44c651c299f324ad7f7fee96bc5f528edc
hikaru-sato1998 <e170e007@gunma-u.ac.jp>
2022-01-07 16:02:38 +0900

    add validation

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -13,11 +13,13 @@
             @csrf
             <div class="title">
                 <h3>タイトル</h3>
-                <input type="text" name="post[title]" placeholder="タイトルを入力">
+                <input type="text" name="post[title]" placeholder="タイトルを入力" value="{{ old('post.title') }}">
+                <p class="title_error" style="color:red">{{ $errors->first('post.title') }}</p>
             </div>
             <div class="body">
                 <h3>本文</h3>
-                <textarea name="post[body]"></textarea>
+                <textarea name="post[body]">{{ old('post.body') }}</textarea>
+                <p class="body_error" style="color:red">{{ $errors->first('post.body') }}</p>
             </div>
             <input type="submit" value="保存">
         </form>
